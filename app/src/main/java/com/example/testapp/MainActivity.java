c90d82e4c013f62261cf55d9668726dcbfd54449
cmbeyer <cbeyer@saintpeters.edu>
2019-02-11 13:29:48 -0500

    Merge branch 'master' of https://github.com/cmbeyer/TestApp
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -1,22 +1,26 @@
 package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.os.Bundle;
+
 import java.sql.*;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
 
     private EditText Name;
     private EditText Password;
     private Button Login;
     private TextView Info;
+    private TextView Register;
     private int counter = 5;
     private boolean check;
 
@@ -29,35 +33,72 @@ public class MainActivity extends AppCompatActivity  {
         Password = (EditText)findViewById(R.id.password);
         Login = (Button)findViewById(R.id.button_id);
         Info = (TextView)findViewById(R.id.tvInfo);
+        Register = (TextView)findViewById(R.id.register);
 
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                connectDB(Name.getText().toString(), Password.getText().toString());
+            public void onClick(View view)
+            {
+                new AsyncCaller().execute();
             }
         });
-    }
 
-    
-    private void connectDB(String userName, String userPassword){
-        try{
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            Connection con=DriverManager.getConnection("jdbc:mysql://69.142.191.249:3306/WebUsers","root","14752369Cb");
-            Statement stmt=con.createStatement();
-
-            ResultSet rs=stmt.executeQuery("Select * from UserName where userName='"+userName+"' and pass='"+userPassword+"'");
-            if(rs.next())
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
             {
-                check=true;
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
             }
-            else
-                {
-                check = false;
-            }
-            validate();
-        }catch(Exception e){ System.out.println(e);}
+        });
+
+
     }
 
+    private class AsyncCaller extends AsyncTask<Void, Void, Boolean>
+    {
+        ProgressDialog pdLoading = new ProgressDialog(MainActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //this method will be running on UI thread
+            pdLoading.setMessage("\tLoading...");
+            pdLoading.show();
+        }
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try{
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+                Connection con=DriverManager.getConnection("jdbc:mysql://69.142.191.249:3306/WebUsers","root","14752369Cb");
+                Statement stmt=con.createStatement();
+
+                ResultSet rs=stmt.executeQuery("Select * from UserName where userName='"+Name.getText().toString()+"' and pass='"+Password.getText().toString()+"'");
+                if(rs.next())
+                {
+                    check=true;
+                }
+                else
+                {
+                    check = false;
+                }
+                validate();
+            }catch(Exception e){ System.out.println(e);}
+
+            return check;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+
+            //this method will be running on UI thread
+
+            pdLoading.dismiss();
+        }
+
+    }
     private void validate(){
         if(check){
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
